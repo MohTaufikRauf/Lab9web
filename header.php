17c639299097c47db6feea416a1ba0f56295537d
Taufikrauf19 <mohtaufikrauf19@mhs.pelitabangsa.ac.id>
2022-06-02 15:02:23 +0700

    add

--- header.php
+++ header.php
@@ -1,18 +1,3 @@
-## NAMA : MOH.TAUFIK RAUF
-## NIM  : 312010151
-## KELAS: TI.20.A1
-
-# Praktikum 9: PHP Modular
-
-## Instruksi Praktikum
-1. Persiapkan text editor misalnya VSCode.
-2. Buat folder baru dengan nama lab9_php_modular pada docroot webserver
-(htdocs)
-3. Ikuti langkah-langkah praktikum yang akan dijelaskan berikutnya. Langkah-langkah Praktikum
-
-Buat file baru dengan nama header.php
-Kemudian masukan kode berikut
-```
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,4 +16,3 @@ media="screen" />
             <a href="about.php">Tentang</a>
             <a href="kontak.php">Kontak</a>
         </nav>
-```
